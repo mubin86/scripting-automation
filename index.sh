946b8b98f3f16fa743ec7855f61cad19c2d3a225
mubin86 <rahman.mmubinur@gmail.com>
2021-07-07 02:23:26 +0600

    variable declaration

--- index.sh
+++ index.sh
@@ -1,3 +1,18 @@
 #! /usr/bin/bash
 
-echo "Hello People!!!!!!!!!!"
+echo "Hello People!!!!!!!!!!" 
+
+#system variable
+#system variable_defined with uppercase and starts with $
+echo $BASH 
+echo $BASH_VERSION
+echo $HOME
+echo current working directory id $PWD
+
+
+#user defined variable
+#normally used with lowercase
+name=Mubin
+10=age #wrong declaration->variable never starts with number
+echo Name is $name
+
