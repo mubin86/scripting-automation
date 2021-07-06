3c20b8937ba0ab0a500989b5f4f4611d7e78a895
mubin86 <rahman.mmubinur@gmail.com>
2021-07-07 02:58:13 +0600

    taking different type of input

--- index.sh
+++ index.sh
@@ -13,6 +13,30 @@ echo current working directory id $PWD
 #user defined variable
 #normally used with lowercase
 name=Mubin
-10=age #wrong declaration->variable never starts with number
+#10=age #wrong declaration->variable never starts with number
 echo Name is $name
 
+
+#taking input from the keyboard
+# echo "Enter Name"
+# read name ##will store the entered input in this variable
+# echo "Entered name is" $name
+
+
+# #multiple input
+# echo "Enter Name1, Name2"
+# read name1 name2 #will store the entered input in this variable
+# echo "name1 is $name1, name2 is $name2"
+
+
+# ##taking input on the same line means same propmt
+# read -p "enter name " name
+# echo "entered name is $name"
+
+
+#taking secret type input like password
+read -p "enter username " uname
+read -sp "enter password " upassword #this will take the input but does not show in the terminal
+echo #this will work as a new line
+echo "username is $uname"
+echo "password is $upassword"
