37b9c056d4e7b02f2118a833a11a18263819cbd7
mubin86 <rahman.mmubinur@gmail.com>
2021-07-19 03:06:06 +0600

    passing argumentwith terminal update

--- index.sh
+++ index.sh
@@ -55,5 +55,12 @@
 # read #if we dont take any variable in input
 # echo "name is $REPLY" #then the output will store in this default variable which is a reserved keyword
 
-###passing arguments in a bash script
+###passing arguments in a bash script with terminal
 echo $1 $2 $3 ' > echo $1 $2 $3' # whenever i start the script with terminal if pass some arguments here the first $1 $2 will work as a parameter to receive those input argu--->it takes the input
+echo $0 $1 $2 $3  ##$0 will represent the executed scrip name 
+
+##parse arguments using an array
+args=("$@") ##args can be any variable name
+echo ${args[0]} ${args[1]} ${args[2]} ${args[3]} ##here the 0th index will indicate the first terminal argument
+echo $@ #these are default variable for array and will work same just like sthe previous line
+echo $# ## wil output the number of arguments passed in the terminal
